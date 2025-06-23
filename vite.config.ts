bff00938a00220f9627897cbbb209e07745ff32c
charan23-ai <kcharanreddy07@gmail.com>
2025-06-23 21:02:56 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -23,3 +23,16 @@ export default defineConfig(({ mode }) => ({
     outDir: "dist",
   }
 }));
+
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
